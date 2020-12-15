a8304367f29096de4247cbef7dc6b784c22b0a40
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2020-12-15 16:17:26 -0500

    objects.js

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 //object literal
+//1st out of the 3 ways to make an object
 let x = {
     name: "Muhtasim",
     job: "Junior Full Stack Developer",
@@ -14,3 +15,38 @@ x.random = "a random value";
 console.log(x.random);
 delete x.random;
 console.log(x.random);
+
+//making a constructor for an object
+//2nd out of the 3 ways to make an object
+//could have params instead of having predefined values for the variables
+function Car(make){
+    this.make = make;
+    this.model = "5 Series";
+    this.year = "1997";
+    this.owner = "Muhtasim"
+}
+
+let myCar = new Car("BMW");
+console.log(myCar.make);
+
+class Student {
+    name;
+    classes;
+    gpa;
+
+    constructor(name, classes, gpa =4){
+        this.name = name,
+        this.classes = classes,
+        this.gpa = gpa
+    }
+
+    talk(){
+        console.log(`Hello, my name is ${this.name}`);
+    }
+
+
+}
+
+let Andy = new Student("Muhtasim", ["Software", "Engineer"]);
+console.log(Andy);
+Andy.talk();
